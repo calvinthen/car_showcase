f78b0c477b48625054a0c9d4577547944fbfe95a
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2024-06-26 17:56:23 +0700

    rapid api 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CustomFilter, SearchBar } from "@/components";
+import { CarCard, CustomFilter, SearchBar } from "@/components";
 import Hero from "@/components/Hero";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default async function Home() {
   const allCars = await fetchCars();
 
-  
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -24,10 +24,32 @@ export default async function Home() {
           
           <SearchBar/>
           <div className="home__filter-container">
-            <CustomFilter title/>
-            <CustomFilter title/>
+            <CustomFilter />
+            <CustomFilter />
           </div>
           
+          {
+            !isDataEmpty ? (
+
+              <section>
+                <div className="home_cars-wrapper">
+                  {allCars?.map((car) => (
+                    <CarCard car={car}/>
+
+                  ))}
+                </div>
+              </section>
+            
+          ):(
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">
+                Oops empty results
+              </h2>
+              <p>
+                {allCars?.message}
+              </p>
+            </div>
+          )}
 
         </div>
      </div>
